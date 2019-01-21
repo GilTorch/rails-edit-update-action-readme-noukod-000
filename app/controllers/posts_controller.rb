7acbecbf295e67b1a0345e10174b5959d9b3d91f
Gilbert Torchon <gilbert@noukod.com>
2019-01-21 19:17:36 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,9 +24,9 @@ class PostsController < ApplicationController
 	end
 
 		def update
-			@article = Article.find(params[:id])
-			@article.update(title: params[:title], description: params[:description])
-			redirect_to article_path(@article)
+			@post = Post.find(params[:id])
+			@post.update(title: params[:title], description: params[:description])
+			redirect_to article_path(@post)
 		end
 
 end
